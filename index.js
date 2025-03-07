456d83c1c01efe680c5fe58965d57a3b444ba19d
John Paul <johntetoo27@gmail.com>
2025-03-07 18:59:06 +0300

    new changes

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+// Multiply two numbers to get 62
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2; // 31 * 2 = 62
+
+// Generate a random integer greater than 0
+let random = Math.floor(Math.random() * 100) + 1; // Random number > 0
+
+// Find the remainder of num3 divided by num4 to get 4
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4; // 28 % 6 = 4
+
+// Find the highest number in a set
+let max = Math.max(5, 10, 15, 20); // Highest value is 20
+
